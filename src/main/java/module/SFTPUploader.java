e67693ca502f9c7ce7f4a4135a17bc759452b7e2
atuwa <atuwasinji@gmail.com>
2019-07-29 17:02:27 +0900

    ホワイトリスト作ったり画像しか転送しないようにしたり

--- src/main/java/module/SFTPUploader.java
+++ src/main/java/module/SFTPUploader.java
@@ -1,16 +1,15 @@
 package module;
 
+import java.awt.image.BufferedImage;
 import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
 import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
 
 import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.JSch;
@@ -20,6 +19,7 @@ import com.jcraft.jsch.SftpException;
 
 import bouyomi.BouyomiProxy;
 import bouyomi.DiscordBOT.BouyomiBOTConection;
+import bouyomi.IAutoSave;
 import bouyomi.IModule;
 import bouyomi.Tag;
 import net.dv8tion.jda.api.entities.Message.Attachment;
@@ -28,22 +28,30 @@ import net.dv8tion.jda.api.entities.Message.Attachment;
  * @author tool-taro.com
  * @see https://qiita.com/tool-taro/items/a3f016a58e3e32675858
  */
-public class SFTPUploader implements IModule,Runnable{
-	public static final long memorySize=1*1024L*1024L*1024L;
+public class SFTPUploader implements IModule,Runnable,IAutoSave{
 	public static final long keepConection=1*60*1000L;
 	public ArrayList<FileData> queue=new ArrayList<FileData>();
 	private boolean conection;
 	//サーバ設定
 	private String host,user,password,dir;
+	private ArrayList<String> whiteList=new ArrayList<String>();
+	private boolean whiteList_seved=false;
+	private boolean log=false;
 	public SFTPUploader(){
 		host=BouyomiProxy.Config.get("SFTPサーバ");
 		user=BouyomiProxy.Config.get("SFTPユーザ");
 		password=BouyomiProxy.Config.get("SFTPパスワード");
 		dir=BouyomiProxy.Config.get("SFTPディレクトリ");
+		try{
+			BouyomiProxy.load(whiteList,"SFTP転送許可ユーザリスト.txt");
+			whiteList_seved=true;
+		}catch(IOException e){
+			e.printStackTrace();
+		}
 	}
 	public void main()
 			throws JSchException,SftpException,FileNotFoundException,IOException{
-		System.out.println("SFTPサーバに接続します...");
+		if(log)System.out.println("SFTPサーバに接続します...");
 		JSch jsch;
 		Session session=null;
 		ChannelSftp channel=null;
@@ -67,33 +75,33 @@ public class SFTPUploader implements IModule,Runnable{
 
 			channel=(ChannelSftp) session.openChannel("sftp");
 			channel.connect();
-			System.out.println("SFTPサーバに接続成功");
+			if(log)System.out.println("SFTPサーバに接続成功");
 			//ディレクトリ移動
-			System.out.println("ターゲットディレクトリに移動します...");
+			if(log)System.out.println("ターゲットディレクトリに移動します...");
 			channel.cd(dir);
-			System.out.println("移動成功\n転送ループに突入しました");
+			if(log)System.out.println("移動成功\n転送ループに突入しました");
 			long t=0;
 			while(true) {
 				if(!queue.isEmpty())synchronized(queue){
-					System.out.println("転送キューは"+queue.size()+"件です\n0件目を取り出します...");
+					if(log)System.out.println("転送キューは"+queue.size()+"件です\n0件目を取り出します...");
 					FileData fd=queue.get(0);
-					System.out.println("0件目をキューから除去します...");
+					if(log)System.out.println("0件目をキューから除去します...");
 					queue.remove(0);
 					try {
-						System.out.println("ファイル名重複を検証します...");
+						if(log)System.out.println("ファイル名重複を検証します...");
 						final String nb=fd.name;
 						if(isExist(channel,fd.name)) {
 							for(int i=1;isExist(channel,fd.name);i++){
 								fd.name=nb+i;
 							}
 						}
-						System.out.println("ファイル名"+fd.name+"で転送を開始します...");
+						if(log)System.out.println("ファイル名"+fd.name+"で転送を開始します...");
 						//アップロード
 						channel.put(fd.is,fd.name);
 					}finally {
 						fd.Final();
 					}
-					System.out.println("転送を完了しました");
+					if(log)System.out.println("転送を完了しました");
 					t=0;
 				}
 				try{
@@ -121,7 +129,7 @@ public class SFTPUploader implements IModule,Runnable{
 			System.out.format("ダウンロードしたファイル=%1$s", new String(bout.toByteArray(), StandardCharsets.UTF_8));
 			*/
 		}finally{
-			System.out.println("接続を切断します...");
+			if(log)System.out.println("接続を切断します...");
 			try {
 				if(bin!=null)try{
 					bin.close();
@@ -132,21 +140,21 @@ public class SFTPUploader implements IModule,Runnable{
 				if(session!=null)try{
 					session.disconnect();
 				}catch(Exception e){}
-				System.out.println("正常に切断しました");
+				if(log)System.out.println("正常に切断しました");
 			}finally {
 				conection=false;
 			}
 		}
 	}
 	private boolean isExist(ChannelSftp channel,String targetFilePath) {
-		System.out.println("ファイル名"+targetFilePath+"を検証します...");
+		if(log)System.out.println("ファイル名"+targetFilePath+"を検証します...");
 		try {
 			channel.lstat(targetFilePath);
 		} catch (SftpException e) {
-			System.out.println("ファイル名"+targetFilePath+"は存在しません");
+			if(log)System.out.println("ファイル名"+targetFilePath+"は存在しません");
 			return false;
 		}
-		System.out.println("ファイル名"+targetFilePath+"は存在します");
+		if(log)System.out.println("ファイル名"+targetFilePath+"は存在します");
 		return true;
     }
 	public static class FileData{
@@ -160,35 +168,36 @@ public class SFTPUploader implements IModule,Runnable{
 		}
 		public void Final(){}
 	}
-	private FileData getData(BouyomiBOTConection bc,Attachment a){
-		if(a.getSize()<memorySize){
-			ByteArrayOutputStream os=new ByteArrayOutputStream(a.getSize());
-			bc.bot.downloadOutputStream(a.getUrl(),os);
-			InputStream is =new ByteArrayInputStream(os.toByteArray());
-			return new FileData(a.getSize(),is,a.getFileName());
-		}else {
-			try {
-				int index=a.getFileName().lastIndexOf('.');
-				String suffix=null;
-				if(index>0&&index<a.getFileName().length())suffix=a.getFileName().substring(index);
-				final File temp=Files.createTempFile("sftp_temp",suffix).toFile();
-				FileOutputStream os=new FileOutputStream(temp);
-				bc.bot.downloadOutputStream(a.getUrl(),os);
-				final FileInputStream is=new FileInputStream(temp);
-				return new FileData(a.getSize(),is,a.getFileName()) {
-					@Override
-					public void Final(){
-						if(is!=null)try{
-							is.close();
-							temp.delete();
-						}catch(IOException e){}
-					}
-				};
-			}catch(IOException e) {
+	private FileData getData(Tag tag, BouyomiBOTConection bc,Attachment a){
+		ByteArrayOutputStream os=new ByteArrayOutputStream(a.getSize());
+		bc.bot.downloadOutputStream(a.getUrl(),os);
+		InputStream is =new ByteArrayInputStream(os.toByteArray());
+		os.reset();
+		try{
+			BufferedImage image=ImageIO.read(is);
+			try{
+				Thread.sleep(1000);
+			}catch(InterruptedException e){
 				e.printStackTrace();
 			}
+			int index=a.getFileName().lastIndexOf('.');
+			String suffix="png";
+			if(index>0&&index+1<a.getFileName().length())suffix=a.getFileName().substring(index+1);
+			if(!ImageIO.write(image,suffix,os))throw new IOException();
+			try{
+				Thread.sleep(1000);
+			}catch(InterruptedException e){
+				e.printStackTrace();
+			}
+		}catch(Exception e){
+			tag.chatDefaultHost("/画像検証失敗(非対応データです)");
+			try{
+				is.close();
+			}catch(IOException e1){}
+			return null;
 		}
-		return null;
+		is =new ByteArrayInputStream(os.toByteArray());
+		return new FileData(a.getSize(),is,a.getFileName());
 	}
 	private boolean check(String... list) {
 		for(String s:list) {
@@ -196,21 +205,69 @@ public class SFTPUploader implements IModule,Runnable{
 		}
 		return false;
 	}
+	private boolean isAdmin(String userid) {
+		return userid.equals("543670181734645781")||//妖花ちゃん
+				userid.equals("544529530866368522")||//黄泉
+				userid.equals("440102258143920128");//天狗さん
+	}
 	@Override
 	public void call(Tag tag){
+		String s=tag.getTag("SFTP許可");
+		if(s!=null&&isAdmin(tag.con.userid)) {
+			if(s.isEmpty()){
+				tag.chatDefaultHost("ユーザIDを指定してください");
+			}else try{
+				Long.parseLong(s);
+				if(whiteList.contains(s)) {
+					tag.chatDefaultHost("登録済です");
+				}else {
+					whiteList.add(s);
+					whiteList_seved=false;
+					tag.chatDefaultHost("登録しました");
+				}
+			}catch(NumberFormatException e) {
+				tag.chatDefaultHost("ユーザIDを指定してください");
+			}
+		}
+		s=tag.getTag("SFTP拒否");
+		if(s!=null&&isAdmin(tag.con.userid)) {
+			if(s.isEmpty()){
+				tag.chatDefaultHost("ユーザIDを指定してください");
+			}else try{
+				Long.parseLong(s);
+				if(whiteList.contains(s)) {
+					whiteList.remove(s);
+					whiteList_seved=false;
+					tag.chatDefaultHost("許可リストから削除しました");
+				}else {
+					tag.chatDefaultHost("登録されていません");
+				}
+			}catch(NumberFormatException e) {
+				tag.chatDefaultHost("ユーザIDを指定してください");
+			}
+		}
+		s=tag.getTag("アップロード許可されてる？","アップロード許可されてる?");
+		if(s!=null) {
+			if(s.isEmpty()){
+				tag.chatDefaultHost("あなたは許可されて"+(whiteList.contains(s)?"います":"いません"));
+			}else try{
+					Long.parseLong(s);
+					String nick=tag.getUserName(s);
+					tag.chatDefaultHost(nick+"さんは許可されて"+(whiteList.contains(s)?"います":"いません"));
+			}catch(NumberFormatException e) {
+				tag.chatDefaultHost("あなたは許可されて"+(whiteList.contains(s)?"います":"いません"));
+			}
+		}
 		if(check(host,user,password,dir))return;
 		if(tag.con.text.indexOf("SFTPアップロード")!=0)return;
 		if(tag.con instanceof BouyomiBOTConection);
 		else return;
 		BouyomiBOTConection bc=(BouyomiBOTConection) tag.con;
-		if(tag.con.userid.equals("543670181734645781")||//妖花ちゃん
-				tag.con.userid.equals("544529530866368522")||//黄泉
-				tag.con.userid.equals("581044447890898964")||//つるぎ
-				tag.con.userid.equals("440102258143920128")){//天狗
+		if(isAdmin(tag.con.userid)||whiteList.contains(tag.con.userid)){
 			tag.con.text="";
 			for(int i=0;i<bc.list.length;i++){
-				System.out.println("DiscordにSFTP転送するべきファイルが投稿されました"+bc.list.length);
-				FileData fd=getData(bc,bc.list[i]);
+				if(log)System.out.println("DiscordにSFTP転送するファイルが投稿されました"+bc.list.length);
+				FileData fd=getData(tag,bc,bc.list[i]);
 				if(fd!=null)synchronized(queue) {
 					queue.add(fd);
 				}
@@ -221,6 +278,8 @@ public class SFTPUploader implements IModule,Runnable{
 					new Thread(this).start();
 				}
 			}
+		}else {
+			tag.chatDefaultHost("あなたは許可されていません");
 		}
 	}
 	@Override
@@ -230,5 +289,19 @@ public class SFTPUploader implements IModule,Runnable{
 		}catch(JSchException|SftpException|IOException e){
 			e.printStackTrace();
 		}
+	}
+	@Override
+	public void shutdownHook() {
+		try{
+			autoSave();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	@Override
+	public void autoSave() throws IOException{
+		if(whiteList_seved)return;
+		BouyomiProxy.save(whiteList,"SFTP転送許可ユーザリスト.txt");
+		whiteList_seved=true;
 	}
 }
