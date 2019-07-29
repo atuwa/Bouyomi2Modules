367fb19384712c93888c5f32243240c9544a7417
atuwa <atuwasinji@gmail.com>
2019-07-29 14:51:05 +0900

    一定サイズ以上になったら別ファイルへ
    他のモジュールより先に処理されるように

--- src/main/java/module/CommentLogger.java
+++ src/main/java/module/CommentLogger.java
@@ -1,5 +1,7 @@
 package module;
 
+import java.io.File;
+
 import bouyomi.BouyomiProxy;
 import bouyomi.IModule;
 import bouyomi.Tag;
@@ -12,10 +14,17 @@ public class CommentLogger implements IModule{
 		String command=BouyomiProxy.Config.get("ログファイル");
 		//0文字だったら無し、それ以外だったらそれ
 		if(command!=null&&!command.isEmpty())logger=new SaveProxyData(command);
-		System.out.println("ログ"+(logger==null?"無し":logger.file));
+		System.out.println("ログ"+(logger==null?"無し":logger.getFile()));
 	}
 	@Override
 	public void call(Tag tag){
-		if(logger!=null) logger.log(tag.con.user+"\t"+tag.con.text+"\t"+tag.con.userid);
+		if(logger!=null) {
+			logger.log(tag.con.user+"\t"+tag.con.text+"\t"+tag.con.userid);
+			String fn=logger.getFile();
+			File f=new File(fn);
+			if(f.length()>1048576) {//1MB以上で次のファイルへ
+				logger.nextFile();
+			}
+		}
 	}
 }
