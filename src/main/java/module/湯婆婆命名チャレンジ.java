78576ff8ffd9c5fa1ded8f2b858ce01b9ce0fce4
atuwa <atuwasinji@gmail.com>
2019-10-31 16:34:56 +0900

    対応パターンを追加

--- src/main/java/module/湯婆婆命名チャレンジ.java
+++ src/main/java/module/湯婆婆命名チャレンジ.java
@@ -13,26 +13,34 @@ public class 湯婆婆命名チャレンジ implements IModule{
 	public void call(Tag tag){
 		int index=tag.con.text.indexOf("湯婆婆「フン。");
 		if(index==0) {
-			int end=tag.con.text.indexOf("というのかい？贅沢な名だねぇ。今からおまえの名前は");
+			ゆばーば(tag,index,"というのかい？贅沢な名だねぇ。今からおまえの名前は","今からおまえの名前は","だ。いいかい、");
+			ゆばーば(tag,index,"というのかい？貧相な名だねぇ。今からあんたの名前は","今からあんたの名前は","だ。");
+			ゆばーば(tag,index,"というのかい？さてはアンチだなオメー","さては","だなオメー");
+			ゆばーば(tag,index,"というのかい？贅沢な名だねぇ。今からおまえの名前は","今からおまえの名前は","だ。嫌かい？");
+		}
+	}
+	private void ゆばーば(Tag tag,int index,String orgEndKey,String startKey,String endKey) {
+		int end=tag.con.text.indexOf(orgEndKey);
+		if(end<0)return;
+		String name=tag.con.text.substring(index+7,end);
+		//tag.chatDefaultHost("デバッグログ："+name);
+		index=tag.con.text.indexOf(startKey);
+		if(index>7+name.length()) {
+			end=tag.con.text.indexOf(endKey);
 			if(end<0)return;
-			String name=tag.con.text.substring(index+7,end);
-			//tag.chatDefaultHost("デバッグログ："+name);
-			index=tag.con.text.indexOf("今からおまえの名前は");
-			if(index>7+name.length()) {
-				end=tag.con.text.indexOf("だ。いいかい、");
-				String newname=tag.con.text.substring(index+10,end);
-				if(DiscordBOT.bots==null||DiscordBOT.bots.isEmpty())return;
-				try {
-					String oldnick=tag.getUserNick(tag.con.userid);
-					Guild g=DiscordBOT.DefaultHost.jda.getGuildById("566942640986390528");
-					Member member=g.getMemberById(tag.con.userid);
-					g.getController().setNickname(member,newname+"("+name+")").queue();
-					tag.chatDefaultHost(oldnick+"→"+newname+"("+name+")");
-				}catch(HierarchyException he) {
-					tag.chatDefaultHost(he.getLocalizedMessage());
-				}catch(Exception e) {
-					e.printStackTrace();
-				}
+			String newname=tag.con.text.substring(index+startKey.length(),end);
+			if(DiscordBOT.bots==null||DiscordBOT.bots.isEmpty())return;
+			if(newname.equals("アンチ"))newname="湯婆婆アンチ";
+			try {
+				String oldnick=tag.getUserNick(tag.con.userid);
+				Guild g=DiscordBOT.DefaultHost.jda.getGuildById("566942640986390528");
+				Member member=g.getMemberById(tag.con.userid);
+				g.getController().setNickname(member,newname+"("+name+")").queue();
+				tag.chatDefaultHost(oldnick+"→"+newname+"("+name+")");
+			}catch(HierarchyException he) {
+				tag.chatDefaultHost(he.getLocalizedMessage());
+			}catch(Exception e) {
+				e.printStackTrace();
 			}
 		}
 	}
